256fc88683a939bacad56667321ed634c27d5dfe
andrelrocha <andrerocha0911@gmail.com>
2023-11-24 12:31:19 -0300

    deploy projeto

---
+++ src/test/java/rocha/andre/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rocha.andre.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
